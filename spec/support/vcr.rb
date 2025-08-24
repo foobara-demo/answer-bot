a89ada5b4ff6a159b6fd4409d7edfcdee9573653
Miles Georgi <azimux@gmail.com>
2025-08-24 15:52:31 -0700

    Strip out Set-Cookie from VCR cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,11 @@ require "vcr"
 
 VCR.configure do |config|
   config.filter_sensitive_data("<SCRUBBED_OPENAI_API_KEY>") { ENV.fetch("OPENAI_API_KEY", nil) }
+  config.before_record do |interaction|
+    # Stripping these out just in-case they contain some kind of auth token.
+    interaction.response.headers.delete("Set-Cookie")
+  end
+
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
